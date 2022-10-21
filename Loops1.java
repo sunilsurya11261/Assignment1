ae0154e37cec99fc52c100185f0c68ace7a9a141
sunilsurya11261 <115568324+sunilsurya11261@users.noreply.github.com>
2022-10-21 14:49:29 +0530

    Add files via upload

---
+++ Loops1.java
@@ -0,0 +1,26 @@
+
+public class Loops1 {
+	public static void main(String args[]) {
+		int n=9;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(i==0&&i==n-1&&i!=0&&j!=0||i==0||i==(n-1)/2||j==0||j==n-1)
+//				if(i==0||j==3||i==3||j==7)	
+				{
+				  System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		
+		
+		
+	}
+
+}
